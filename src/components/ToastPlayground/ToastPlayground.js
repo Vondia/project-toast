5c9d05b45aaf2df5210e50586eda055656e1eeab
vondia <pimvdbosch@gmail.com>
2024-01-30 10:38:17 +0100

    part 3 finished

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,16 +4,60 @@ import Button from '../Button';
 import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
+import ToastShelf from '../ToastShelf/ToastShelf';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
-    const [isToastVisible, setIsToastVisible] = React.useState(false);
+    const [toasts, setToasts] = React.useState([
+        {
+            id: crypto.randomUUID(),
+            message: 'Oh no!',
+            variant: 'error',
+        },
+        {
+            id: crypto.randomUUID(),
+            message: 'This is a notice.',
+            variant: 'notice',
+        },
+        {
+            id: crypto.randomUUID(),
+            message: 'This is a warning.',
+            variant: 'warning',
+        },
+        {
+            id: crypto.randomUUID(),
+            message: 'This is a success.',
+            variant: 'success',
+        }
+    ]);
     const [message, setMessage] = React.useState('');
     const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
 
-function handleDismiss() {
-    setIsToastVisible(false);
+function handleCreateToast(event) {
+    event.preventDefault();
+    const nextToasts = [
+        ...toasts,
+        {
+            id: crypto.randomUUID(),
+            message,
+            variant,
+        },
+    ];
+
+    setToasts(nextToasts);
+
+    setMessage('');
+
+    setVariant(VARIANT_OPTIONS[0]);
+    };
+
+function handleDismiss(id) {
+    const nextToasts = toasts.filter(toast => {
+        return toast.id !== id;
+    })
+
+    setToasts(nextToasts)
 }
   return (
     <div className={styles.wrapper}>
@@ -22,18 +66,19 @@ function handleDismiss() {
         <h1>Toast Playground</h1>
       </header>
 
-    {isToastVisible && <Toast variant={variant} handleDismiss={handleDismiss}>
-    {message}
-    </Toast>}
+  <ToastShelf
+    toasts={toasts}
+    handleDismiss={handleDismiss}
+  />
 
-      <div className={styles.controlsWrapper}>
+      <form className={styles.controlsWrapper} onSubmit={handleCreateToast}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
             style={{ alignSelf: 'baseline' }}
           >
-            Message
+            Messagez
           </label>
           <div className={styles.inputWrapper}>
             <textarea id="message" value={message} onChange={event => { setMessage(event.target.value)}} className={styles.messageInput} />
@@ -68,10 +113,10 @@ function handleDismiss() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button onClick={() => {setIsToastVisible(true)}}>Pop Toast!</Button>
+            <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
